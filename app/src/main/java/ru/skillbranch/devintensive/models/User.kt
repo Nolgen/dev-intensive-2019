33b30e8ae9d2b519f726f60c72fd6a6491885008
Nolgen <nolgen@ya.ru>
2019-06-29 20:36:02 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -34,6 +34,34 @@ data class User (
             return User(id= "$lastId", firstName = firstName, lastName = lastName)
         }
     }
-}
 
+    data class Builder(
+        var id: String = "",
+        var firstName: String? = null,
+        var lastName: String? = null,
+        var avatar: String? = null,
+        var rating: Int = 0,
+        var respect: Int = 0,
+        var lastVisit: Date? = Date(),
+        var isOnline: Boolean = false) {
+
+            fun id(id: String) = apply { this.id = id }
+            fun firstName(firstName: String?) = apply { this.firstName = firstName }
+            fun lastName(lastName: String?) = apply { this.lastName = lastName }
+            fun avatar(avatar: String?) = apply { this.avatar = avatar }
+            fun rating(rating: Int = 0) = apply { this.rating = rating }
+            fun respect(respect: Int = 0) = apply { this.respect = respect }
+            fun lastVisit(lastVisit: Date = Date()) = apply { this.lastVisit = lastVisit }
+            fun isOnline(isOnline: Boolean = false) = apply { this.isOnline = isOnline }
+            fun build() = User(
+                id = id,
+                firstName = firstName,
+                lastName = lastName,
+                avatar = avatar,
+                rating = rating,
+                respect = respect,
+                lastVisit = lastVisit,
+                isOnline = isOnline)
+        }
+}
 
