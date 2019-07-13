8ca837519e3d12a53d926d1ad78832e42de57434
Nolgen <nolgen@ya.ru>
2019-07-13 11:10:57 +0300

    hometask_3 Реализованы методы и лог жизненного цикла

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,7 +1,6 @@
 package ru.skillbranch.devintensive.utils
 
 import android.os.Build
-import android.support.annotation.RequiresApi
 
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
@@ -29,7 +28,6 @@ object Utils {
         return if (res == "") null else res
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
     fun transliteration(payload: String, divider: String = " "): String {
         val transTable: Map<String,String> = mapOf("а" to "a","б" to "b","в" to "v","г" to "g","д" to "d","е" to "e",
             "ё" to "e","ж" to "zh","з" to "z","и" to "i","й" to "i","к" to "k","л" to "l","м" to "m","н" to "n",
