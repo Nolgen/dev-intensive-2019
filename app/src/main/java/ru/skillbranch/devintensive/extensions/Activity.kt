d7ab275084fb99df7dca61ec85b24fd71b9e0b3f
Nolgen <nolgen@ya.ru>
2019-07-14 21:33:50 +0300

    hometask_3 Сделаны задания 6,7

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -2,10 +2,30 @@ package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun Activity.hideKeyboard() {
     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(findViewById<View>(android.R.id.content).windowToken, 0);
+    val token = findViewById<View>(android.R.id.content).windowToken
+    imm.hideSoftInputFromWindow(token,0)
+}
+
+fun Activity.isKeyboardClosed():Boolean {
+    return isKeyboardOpen().not()
+}
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val rootView = findViewById<View>(android.R.id.content)
+
+    val visibleBounds = Rect()
+    rootView.getWindowVisibleDisplayFrame(visibleBounds)
+
+    val appHeight = rootView.height            //высота всего приложения
+    val visibleHeight = visibleBounds.height() //высота видимой части приложения
+
+    // True если скрыто более 15% приложения
+    return (appHeight-visibleHeight) > (appHeight * 0.15)
+
 }
--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.models
 
+import androidx.core.text.isDigitsOnly
+
 class Bender(var status:Status = Status.NORMAL, var question: Question = Question.NAME) {
 
     fun askQuestion():String = when(question){
@@ -12,7 +14,12 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
     }
 
     fun listenAnswer(answer:String): Pair<String, Triple<Int,Int,Int>> {
-        if (question == Question.IDLE) return "${question.question}" to status.color
+        if (question == Question.IDLE) return question.question to status.color
+
+        // Валидация
+        val validate = validateAnswer(answer,question)
+        if(validate != "") return validate to status.color
+
         if(question.answers.contains(answer.toLowerCase())) {
             question = question.nextQuestion()
             return "Отлично - ты справился\n${question.question}" to status.color
@@ -47,10 +54,12 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
     enum class Question(val question: String, val answers: List<String>) {
         NAME("Как меня зовут?", listOf("бендер", "bender")) {
             override fun nextQuestion(): Question = PROFESSION
+
         },
         PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
             override fun nextQuestion(): Question = MATERIAL
-        },
+
+            },
         MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
             override fun nextQuestion(): Question = BDAY
         },
@@ -65,5 +74,27 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
         };
 
         abstract fun nextQuestion():Question
+    }
+
+    fun validateAnswer(answer: String, question: Question):String {
+        return when(question) {
+            Question.NAME -> if (answer.isNotEmpty() && answer[0] == answer[0].toUpperCase()) ""
+                else "Имя должно начинаться с заглавной буквы\nКак меня зовут?"
+
+            Question.PROFESSION -> if (answer.isNotEmpty() && answer[0] != answer[0].toUpperCase()) ""
+                else "Профессия должна начинаться со строчной буквы\nНазови мою профессию?"
+
+            Question.MATERIAL -> if (answer.isNotEmpty() && ! answer.contains(Regex("[0-9]") )) ""
+                else "Материал не должен содержать цифр\nИз чего я сделан?"
+
+            Question.BDAY -> if (answer.isNotEmpty() && answer.isDigitsOnly()) ""
+                else "Год моего рождения должен содержать только цифры\nКогда меня создали?"
+
+            Question.SERIAL -> if (answer.isNotEmpty() && answer.isDigitsOnly() && answer.length == 7) ""
+                else "Серийный номер содержит только цифры, и их 7\nМой серийный номер?"
+
+            Question.IDLE ->  ""
+        }
+
     }
 }
